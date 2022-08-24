2929a9d9f6e857a26c4c674e79a510b69cef094a
sourabhpanda243 <63732583+sourabhpanda243@users.noreply.github.com>
2022-08-24 17:09:31 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import pyodbc
+print("Hello from Sourabh")
+server = 'sgsin01sql001v.database.windows.net'
+database = 'control-tower-test-db'
+username = 'sourabh'
+password = '{admin@123}'   
+#driver= '{FreeTDS}'
+driver= '{ODBC Driver 18 for SQL Server}'
+
+#conn = pyodbc.connect('DRIVER={FreeTDS};SERVER=sgsin01sql001v.database.windows.net;PORT=1433;DATABASE=control-tower-test-db;UID=sourabh;PWD=admin@123', autocommit=True)
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+#        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        cursor.execute("select * from dbo.Persons")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1])+ " " + str(row[2]))
+            row = cursor.fetchone()
